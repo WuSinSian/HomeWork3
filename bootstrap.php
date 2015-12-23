46eb382f670fe139ff6b5a8048b4cf1dbe5a94c6
WuSinSian <gb80243@gmail.com>
2015-12-23 14:31:13 +0800

    set autoload

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 require	__DIR__.'/vendor/autoload.php';
-require 'vendor/autoload.php';
+//require 'vendor/autoload.php';
 define('DB_HOST', 'localhost');
 define('DB_PORT', 33060);
 define('DB_DATABASE', 'laravel_local');
