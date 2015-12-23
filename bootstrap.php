c4822a9c56e0128a350db4b3751ef3a80719419a
WuSinSian <gb80243@gmail.com>
2015-12-23 14:21:16 +0800

    default Carbon project

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require	__DIR__.'/vendor/autoload.php';
+require 'vendor/autoload.php';
+define('DB_HOST', 'localhost');
+define('DB_PORT', 33060);
+define('DB_DATABASE', 'laravel_local');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
